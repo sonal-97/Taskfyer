d960e605f62e7449c38d0dcf58cbd4b3e09f02ca
sonal-97 <sonalpathak84058@gmail.com>
2025-02-17 00:58:36 +0530

    the sidebar logout button changes

--- client/app/Components/Sidebar/Sidebar.tsx
+++ client/app/Components/Sidebar/Sidebar.tsx
@@ -5,15 +5,21 @@ import { useUserContext } from "@/context/userContext";
 
 function Sidebar() {
   const { logoutUser } = useUserContext();
+  
   return (
-    <div className="w-[20rem] mt-[5rem] h-[calc(100%-5rem)] fixed right-0 top-0 bg-[#f9f9f9] flex flex-col">
+    <div className="w-[20rem] mt-[5rem] h-screen fixed right-0 top-0 bg-[#f9f9f9] flex flex-col justify-between">
+      {/* Profile Section */}
       <Profile />
+      
+      {/* Radial Chart Section */}
       <div className="mt-4 mx-6">
         <RadialChart />
       </div>
 
+      {/* Sign Out Button - Always Visible in Bottom Left */}
       <button
-        className="mt-auto mb-6 mx-6 py-4 px-8 bg-[#EB4E31] text-white rounded-[50px] hover:bg-[#3aafae] transition duration-200 ease-in-out"
+        className="absolute bottom-6 left-6 py-4 px-8 bg-[#EB4E31] text-white rounded-[50px] 
+                   hover:bg-[#3aafae] transition duration-200 ease-in-out"
         onClick={logoutUser}
       >
         Sign Out
