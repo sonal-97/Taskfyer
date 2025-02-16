623fdd7c6804c046820dd3793d72df34d7d61b7c
sonal-97 <sonalpathak84058@gmail.com>
2025-02-17 01:11:46 +0530

    making the sidebar scrollable

--- client/app/Components/Sidebar/Sidebar.tsx
+++ client/app/Components/Sidebar/Sidebar.tsx
@@ -3,29 +3,25 @@ import Profile from "../Profile/Profile";
 import RadialChart from "../RadialChart/RadialChart";
 import { useUserContext } from "@/context/userContext";
 
-const Sidebar: React.FC = () => {
+function Sidebar() {
   const { logoutUser } = useUserContext();
-
   return (
-    <div className="w-[20rem] h-screen fixed right-0 top-0 bg-[#f9f9f9] flex flex-col justify-between p-6">
-      {/* Profile Section */}
+    <div
+      className="w-[20rem] mt-[5rem] h-[calc(100%-5rem)] fixed right-0 top-0 bg-[#f9f9f9] flex flex-col overflow-y-auto"
+    >
       <Profile />
-
-      {/* Radial Chart Section */}
-      <div className="mt-4">
+      <div className="mt-4 mx-6">
         <RadialChart />
       </div>
 
-      {/* Sign Out Button - Always Visible in Bottom Left */}
       <button
-        className="absolute bottom-6 left-6 py-3 px-6 bg-[#EB4E31] text-white rounded-full 
-                   hover:bg-[#3aafae] transition duration-200 ease-in-out shadow-lg"
+        className="mt-auto mb-6 mx-6 py-4 px-8 bg-[#EB4E31] text-white rounded-[50px] hover:bg-[#3aafae] transition duration-200 ease-in-out"
         onClick={logoutUser}
       >
         Sign Out
       </button>
     </div>
   );
-};
+}
 
 export default Sidebar;
